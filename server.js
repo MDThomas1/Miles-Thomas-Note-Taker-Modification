8d83cf36ccc4c57a15803626e0c8d237d53ba127
Miles Thomas <milesman2013@gmail.com>
2022-10-26 16:52:50 +1100

    get html pages and download path

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = 3001
@@ -8,7 +9,13 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 
 app.listen(PORT, () => {
