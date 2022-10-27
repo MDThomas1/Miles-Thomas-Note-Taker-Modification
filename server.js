e37402f690a86bdf264fb899d8f08a4672199652
Miles Thomas <milesman2013@gmail.com>
2022-10-27 11:39:53 +1100

    screenshot download

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Importing and establishing necessary information
 const express = require('express');
 const path = require('path');
+const util = require('util')
+const fs = require('fs')
 const db = require('./db/db.json');
 
-const app = express();
-const PORT = 3001;
+const app = express.Router();
+const PORT = process.env.PORT || 3001;
 
 // Middleware to allow the server to read file types
 app.use(express.urlencoded({ extended: true }));
@@ -13,23 +15,31 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Get request to pull up the index.html page
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 //Get request to pull up the notes.html page
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 // Get request to pull up information from the db.json page
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', function (req, res) {
     res.json(db)
+    res
 });
 
-// Post request to utilise the informtion pushed from the db.json page
-app.post('/api/notes', (req,res) => {
+// Post request to utilise the information pushed from the db.json page
+app.post('/api/notes', function (req, res) {
     console.log('Saved notes retrieved')
+    const {title, text} = req.body
+    if (title && text) {
+        const newNote = {
+            title,
+            text
+        }
+    }
 });
 
 
